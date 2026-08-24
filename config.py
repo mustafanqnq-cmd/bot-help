22c5449d2d6bb0788ace45cc1a1c53d05a9e6d4b
Mustafa Al-Sarmadi <mustafanqnq@gmail.com>
2026-08-24 16:46:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# ستقوم بوضع هذه القيم داخل إعدادات Variables في Railway
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-API_ID = int(os.environ.get("API_ID", 0)) 
-API_HASH = os.environ.get("API_HASH", "")
-FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "") # معرف قناة الاشتراك
